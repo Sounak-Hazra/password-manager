e038b3f176f572d149dbee979f6d3a4c797fdd1c
Sounak-Hazra <hazrasounak87@gmail.com>
2024-07-31 22:44:30 +0530

    incresing responsiveness for mobiles

--- app/passwordmanager/home/page.js
+++ app/passwordmanager/home/page.js
@@ -15,7 +15,7 @@ const page = () => {
                     </div>
             </div>
             <div className='md:px-7 px-2 md:mb-0 mb-4'>
-                <div className='bg-yellow-200 md:px-3 p-2 '>
+                <div className='bg-yellow-200 md:px-3 p-2 mb-28 '>
                     Note : Your privacy and security are our top priorities. Our password manager uses advanced encryption to ensure that your passwords and sensitive information are kept safe. We employ strong AES-256 encryption to protect your data both at rest and in transit.
                     To further safeguard your information, our app features multi-factor authentication (MFA), which adds an extra layer of protection. Our design ensures that your data remains confidential, and we do not have access to your master password or any of your stored information.
                     Regular updates and security reviews are conducted to address potential vulnerabilities and enhance protection. For optimal security, we recommend using unique passwords for each of your accounts and enabling MFA wherever possible.
--- app/passwordmanager/savedpasswords/page.js
+++ app/passwordmanager/savedpasswords/page.js
@@ -23,9 +23,13 @@ const page = () => {
             method: "GET",
         })
         const data = await responce.json()
-        if (data) {
+        if (data.data) {
             setpasswords(data.data)
         }
+        else {
+            setpasswords([])
+        }
+        console.log(passwords)
     }
     const handledelete = async (e) => {
         const data = {appid:e.id}
@@ -72,46 +76,46 @@ const page = () => {
     
     return (
         <>
-            <div className={`h-screen ${makeblur ? "makeblur" : ""} simple-animation`}>
+            <div className={`h-screen ${makeblur ? "makeblur" : ""} simple-animation md:w-auto w-screen`}>
                 <Nav />
 
-                <div className=' w-full custom-height p-4'>
+                {passwords.length === 0 ? <div className='w-full flex justify-center items-center mt-8 text-xl font-semibold'>No Data saved to show </div> : <div className=' w-full custom-height p-4'>
                     <div className=' w-full h-full overflow-y-auto mt-4'>
                         <table className=' w-full relative'>
                             <thead className='w-full  '>
                                 <tr className=' '>
-                                    <th className='text-lg w-[200px] ' scope="col">App name</th>
-                                    <th className='text-lg w-fit' scope="col"><div ><span className='mr-2'>Password</span>
+                                    <th className='md:text-lg text-sm md:w-[200px] ' scope="col">App name</th>
+                                    <th className='md:text-lg w-fit' scope="col"><div ><span className='mr-2'>Password</span>
                                         <button><img onClick={handleclick} className='w-4' src="/eye.svg" alt="" /></button></div>
                                     </th>
-                                    <th className='text-lg ' scope="col">External details</th>
-                                    <th className='text-lg '>Delete/Edit</th>
+                                    <th className='md:text-lg ' scope="col">External details</th>
+                                    <th className='md:text-lg '>Delete/Edit</th>
                                 </tr>
                             </thead>
                             <tbody className='w-full'>
-                                {passwords.map((e) => {
+                                {passwords == [] ? <div>No Data saved to show </div> : passwords.map((e) => {
                                     return <tr key={e.id} className='w-full'>
-                                        <td className='text-center w-10 h-12'>
+                                        <td className='text-center md:w-10 h-12'>
                                             <div className='overflow-y-auto overflow-x-hidden scrollbar-thin h-full'>
-                                                <span className='w-[200px] block break-all'>
+                                                <span className='md:w-[200px] text-xs md:text-base block break-all'>
                                                     {e.appname}
                                                 </span>
                                             </div>
                                         </td>
 
-                                        <td className='text-center w-10 h-12 '><div className='overflow-y-auto overflow-x-hidden scrollbar-thin h-full'>
-                                            <span className={`${showpassword ? "" : "notshow"} w-[200px] block break-all`}>
+                                        <td className='text-center  md:w-10 h-12 '><div className='overflow-y-auto overflow-x-hidden scrollbar-thin h-full'>
+                                            <span className={`${showpassword ? "" : "notshow"} md:w-[200px] text-xs md:text-base block break-all`}>
                                                 {e.password}
                                             </span>
                                         </div></td>
-                                        <td className='text-center w-15 h-12 '><div className='overflow-y-auto overflow-x-hidden scrollbar-thin h-full'>
-                                            <span className='w-full block break-all'>
+                                        <td className='text-center  md:w-15 h-12 '><div className='overflow-y-auto overflow-x-hidden scrollbar-thin h-full'>
+                                            <span className='w-full text-xs md:text-base block break-all'>
                                                 {e.externaldetails}
                                             </span>
                                         </div></td>
-                                        <td className='text-center w-15 h-12 '><div className='overflow-y-auto overflow-x-hidden scrollbar-thin h-full flex justify-around'>
-                                            <button disabled={changePasswordpage} onClick={()=>notify(e)}><img  src="/delete.svg" alt="" /></button>
-                                            <button disabled={changePasswordpage} onClick={()=>handleEdit(e)} ><img  src="/edit.svg" alt="" /></button>
+                                        <td className='text-center  md:w-15 h-12 '><div className='overflow-y-auto overflow-x-hidden scrollbar-thin h-full flex justify-center gap-1 md:justify-around'>
+                                            <button disabled={changePasswordpage} onClick={() => notify(e)}><img src="/delete.svg" alt="" /></button>
+                                            <button disabled={changePasswordpage} onClick={() => handleEdit(e)} ><img src="/edit.svg" alt="" /></button>
                                         </div></td>
                                     </tr>
                                 })}
@@ -120,9 +124,9 @@ const page = () => {
                             </tbody>
                         </table>
                     </div>
-                </div>
+                </div>}
             </div>
-            <div className={`${changePasswordpage ? "" : "hidden"} bg-white border-[.5px] simple-animation  border-gray-500 absolute absolute-item p-4`}>
+            <div className={`${changePasswordpage ? "" : "hidden"} bg-white border-[.5px] simple-animation customWidth md:w-auto  border-gray-500 absolute absolute-item p-4`}>
                <button onClick={handleeditorpage} > <img src={"/crossicon.svg"} className='absolute top-2 right-2' alt="" /></button>
                 <Changepassworddisplay data={props} makeblur={makeblur} setmakeblur={setmakeblur} changePasswordpage={changePasswordpage} setchangePasswordpage={ setchangePasswordpage} passwordUpdated={passwordUpdated} setpasswordUpdated={setpasswordUpdated} />
             </div>
