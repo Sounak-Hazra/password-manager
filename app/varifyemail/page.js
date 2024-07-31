329f405d09fb7495cab7d1e586382b09d1f8a8d4
Sounak-Hazra <hazrasounak87@gmail.com>
2024-07-31 17:10:25 +0530

    first time commiting

---
+++ app/varifyemail/page.js
@@ -0,0 +1,30 @@
+"use client"
+import { useSearchParams } from 'next/navigation'
+
+const page = () => {
+    const searchParams = useSearchParams()
+    const tokenValue = searchParams.get('token')
+
+
+    const handleClick = async () => {
+        const response = await fetch("/api/user/verifyemail", {
+            method: "POST",
+            body: JSON.stringify({ verifytoken: tokenValue }),
+        })
+        const finaldata = await response.json()
+        if (finaldata.success) {
+            alert("Verification successfull")
+            window.location.href = "/login"
+        }
+    }
+
+    return (
+        <>
+            <div className='w-screen h-screen flex items-center justify-center'>
+                <button onClick={handleClick} className='w-fit h-fit px-5 py-2 bg-green-500 rounded-sm font-bold text-white'>VERIFY</button>
+            </div>
+        </>
+    )
+}
+
+export default page
